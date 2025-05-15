974ed823fac616507ebd4bcb8544bd8c79026939
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2025-05-15 15:21:14 +0100

    Fixing deploy bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/Memory-Card-TOP/',
 	plugins: [react()],
+	base: "/Memory-Card-TOP/",
 })
